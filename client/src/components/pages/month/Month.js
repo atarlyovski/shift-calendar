b447f1a6c219dfc94a3779850cab4126d872b825
Alexander Tarlyovski <alexander.tarlyovski@questio.solutions>
2020-03-18 20:38:30 +0200

    Make the Month view swipeable

--- client/src/components/pages/month/Month.js
+++ client/src/components/pages/month/Month.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import SwipeableViews from 'react-swipeable-views';
+
 import moment from '../../../moment-with-locales.custom';
 import MonthElement from './MonthElement';
 import CustomDate from '../../../CustomDate';
@@ -7,14 +9,15 @@ import { useLocale } from '../../../hooks/useLocale';
 import './Month.css';
 
 const Month = () => {
-    let month,
+    let months,
         weeks,
         now = moment(),
         nowFormatted = now.format("YYYY-M-D"),
         locale = useLocale(),
         daysOfWeek;
 
-    const [monthOffset, setMonthOffset] = useState(0);
+    const [index, setIndex] = useState(1);
+    const monthOffsets = [-1, 0, 1];
 
     const getMonthDays = (currentMoment) => {
         let now = moment(currentMoment);
@@ -76,61 +79,80 @@ const Month = () => {
         return headings;
     }
 
-    weeks = getMonthDays(moment(now).add(monthOffset, "month"));
-    month = moment(now).add(monthOffset, "month").locale(locale).format("MMMM");
+    weeks = monthOffsets.map(offset => 
+        getMonthDays(moment(now).add(offset, "month"))
+    );
+
     daysOfWeek = getDaysOfWeek(now);
 
-    let rows = weeks.map((days, i) => {
-        return (
-            <tr key={i}>
-                {days.map(day => {
-                    return (
-                        day.type === "day" ? <MonthElement 
-                            className="column"
-                            key={day.date.toFormattedString()}
-                            date={day.date}
-                            isToday={day.date.toFormattedString() === nowFormatted} />
-                            :
-                            <td key="placeholder"
-                                className="MonthFirstWeekPlaceholder"
-                                colSpan={day.colSpan}></td>
-                    )
-                })}
-            </tr>
-        )
-    })
+    months = monthOffsets.map(offset => 
+        moment(now).add(offset, "month").locale(locale).format("MMMM")
+    );
+
+    let rows = weeks.map(monthWeeks => {
+        return monthWeeks.map((days, i) => {
+            return (
+                <tr key={i}>
+                    {days.map(day => {
+                        return (
+                            day.type === "day" ? <MonthElement 
+                                className="column"
+                                key={day.date.toFormattedString()}
+                                date={day.date}
+                                isToday={day.date.toFormattedString() === nowFormatted} />
+                                :
+                                <td key="placeholder"
+                                    className="MonthFirstWeekPlaceholder"
+                                    colSpan={day.colSpan}></td>
+                        )
+                    })}
+                </tr>
+            )}
+        )}
+    )
 
     return (
-        <div className="Month columns">
-            <div className="column is-8 is-offset-2">
-                <div className="month-heading">
-                    {monthOffset >= 0 ?
+        <div className="Month">
+            <div className="month-heading columns">
+                <div className="column is-8 is-offset-2">
+                    {index >= 1 ?
                         <div className="prev-month"
-                            onClick={() => setMonthOffset(o => o - 1)}>
+                            onClick={() => setIndex(o => o - 1)}>
                         </div> : false
                     }
                     <h2 className="title">
-                        <span>{month}</span>
+                        <span>{months[index]}</span>
                     </h2>
-                    {monthOffset <= 0 ?
+                    {index <= 1 ?
                         <div className="next-month"
-                            onClick={() => setMonthOffset(o => o + 1)}>
+                            onClick={() => setIndex(o => o + 1)}>
                         </div> : false
                     }
                 </div>
-                <div className="table-container">
-                    <table className="MonthTable table is-bordered is-fullwidth is-narrow">
-                        <thead>
-                            <tr>
-                                {daysOfWeek}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {rows}
-                        </tbody>
-                    </table>
-                </div>
             </div>
+            <SwipeableViews
+                index={index}
+                onChangeIndex={setIndex}
+            >
+                {monthOffsets.map((monthOffset, i) => {
+                    return <div className="columns" key={i}>
+                        <div className="MonthTable-container column is-8 is-offset-2">
+                            <div className="table-container">
+                                <table className="MonthTable table is-bordered is-fullwidth is-narrow">
+                                    <thead>
+                                        <tr>
+                                            {daysOfWeek}
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        {rows[i]}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                })}
+            </SwipeableViews>
         </div>
     )
 };
