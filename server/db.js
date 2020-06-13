eecf6835552b2cabc6d07d21af38d15fcd8e79e5
Alexander Tarlyovski <the.christian.666@gmail.com>
2020-06-13 15:06:53 +0300

    Switch storage to Europe-West6 for lowest latency

--- server/db.js
+++ server/db.js
@@ -6,7 +6,7 @@ const GcloudAdapter = require('./GcloudAdapter');
 const adapter = new GcloudAdapter('db.json', {
     projectId: "a-shift-calendar",
     keyFilename: "./A Shift Calendar-c83d3128678d.json",
-    bucketName: "a-shift-calendar-bucket-america",
+    bucketName: "a-shift-calendar-db-eu-w6",
 })
 
 const db = low(adapter);
