ca2a02af545b5423b21858ec06e863abe05acc8b
Alexander Tarlyovski <the.christian.666@gmail.com>
2026-04-26 21:34:23 +0300

    Add deploy hook for certbot

---
+++ deploy-hook.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# This script is intended to be used as a post-deploy hook when certbot renews the SSL certificate in order for the change to take effect.
+# It should be placed in the /etc/letsencrypt/renewal-hooks/deploy/ directory and made executable.
+
+pm2 restart a-shift-calendar
+pm2 save
